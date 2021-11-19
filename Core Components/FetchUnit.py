9bfba393e45eeb3cf3776192f7f09ca84bf6ed81
HamzaRazaQureshi <76109880+HamzaRazaQureshi@users.noreply.github.com>
2021-11-19 12:46:32 +0500

    Update FetchUnit.py

--- Core Components/FetchUnit.py
+++ Core Components/FetchUnit.py
@@ -1,31 +1,31 @@
-# from myhdl import *
-# import random
+from myhdl import *
+import random
 
 
-# randrange = random.randrange
+randrange = random.randrange
 
-# @block
-# def mux(z, a, b, sel):
-#     @always_comb
-#     def comb():
-#         if sel == 0:
-#             z.next = a
-#         else:
-#             z.next = b
+@block
+def mux(z, a, b, sel):
+    @always_comb
+    def comb():
+        if sel == 0:
+            z.next = a
+        else:
+            z.next = b
 
-#     return comb
+    return comb
 
-# @block
-# def adder(x,in1):
-#     @always_comb
-#     def adder_op():
-#         x.next = out + 4
+@block
+def adder(x,in1):
+    @always_comb
+    def adder_op():
+        x.next = out + 4
     
-#     return adder_op
+    return adder_op
 
-# @block
-# def pc_counter(out0, out1, in0):
-#     @always_comb
-#     def pc_count():
-#         out0[24] = in0 + 4
-#         out1 = in0 + 1
+@block
+def pc_counter(out0, out1, in0):
+    @always_comb
+    def pc_count():
+        out0[24] = in0 + 4
+        out1 = in0 + 1
